289b9415b6a3636d033dcb449034ce1cfc942749
hasnain_raza_rajpoot <nainirajpoot07@gmail.com>
2025-10-04 16:16:16 +0500

    added CTpool

--- main.c
+++ main.c
@@ -1,16 +1,46 @@
 #include "utility.h"
 
-#define server_port 80
+#define server_port 8000
+atomic_int ServerRunning = 1;
 
+
+void* handle_client(void *arg){
+    while(1){
+        pthread_mutex_lock(&QueMutuex);
+        if(!queue_is_empty(ClntQue) and ServerRunning){
+            pthread_cond_wait(&QueCV);
+        }
+
+        if(!ServerRunning and queue_is_empty(ClntQue)){
+            pthread_mutex_unlock(&QueMutuex);
+            break;    
+        }
+
+        int client_fd;
+        queue_dequeue(ClntQue,&client_fd);
+        pthread_mutex_unlock(&QueMutuex);
+    }
+    return NULL;
+}
+
+void* handle_signint(void *arg){
+    ServerRunning = 0;
+    pthread_cond_broadcast(&QueCV);
+}
 
 int main(){
     int server_fd, client_fd;
     struct sockaddr_in address;
     socklen_t addrlen = sizeof(address);
+    pthread_t ClienThreadPool[ThreadPoolSize];
+
+    signal(SIGINT,handle_signint);
+
     if((server_fd = socket(AF_INET,SOCK_STREAM,0))==0){
         perror("Failed to start the server\n");
         exit(0);
     }
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(server_port);
@@ -25,12 +55,18 @@ int main(){
         exit(0);
     }
     ClntQue = queue_init(sizeof(int));
-    while(1){
+    for(int i=0;i<ThreadPoolSize;i++){
+        pthread_create(&ClienThreadPool[i],NULL,&handle_client,NULL);
+    }
+    while(ServerRunning){
         client_fd=accept(server_fd,(struct sockaddr*)&address,&addrlen);
         if(client_fd<0){
+            if(!ServerRunning)
+                break;
             perror("Failed to load the client_fd");
             continue;
         }
+
         pthread_mutex_lock(&QueMutuex);
         queue_enqueue(ClntQue,&client_fd);
         pthread_cond_signal(&QueCV);
@@ -38,6 +74,11 @@ int main(){
         printf("\n\n\n--------------------------------------\n\n");
         queue_print(ClntQue,print_int);
         printf("\n\n\n--------------------------------------\n\n");
+
+    }
+    close(server_fd);
+    for(int i=0;i<ThreadPoolSize;i++){
+        pthread_join(ClienThreadPool[i],NULL);
     }
 
     printf("The value %d\n",global);
--- utility.h
+++ utility.h
@@ -6,12 +6,22 @@
 #include <arpa/inet.h>
 #include "queue.c"
 
+#define ThreadPoolSize 4
 
 Queue* ClntQue;
 pthread_mutex_t QueMutuex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t QueCV = PTHREAD_COND_INITIALIZER;
 
 
+
 void print_int(const void* data) {
     printf("%d", *(int*)data);
+}
+
+void print_string(const void* data) {
+    printf("\"%s\"", (char*)data);
+}
+
+void print_float(const void* data) {
+    printf("%.2f", *(float*)data);
 }
